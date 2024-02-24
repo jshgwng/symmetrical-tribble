15bdd8ae050ce7839af742e08456c944e377380f
JOSHUA OGWANG <joshua.ogwang@hotmail.com>
2024-02-24 10:43:12 +0300

    first commit

---
+++ src/test/java/com/smartapplicationsgroup/canteen/CanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartapplicationsgroup.canteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
